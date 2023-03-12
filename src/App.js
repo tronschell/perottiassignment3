f9b329f4bfdd90974f7471603707f94a2ff8840c
Tron Schell <tys6181@rit.edu>
2023-03-12 18:30:03 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,108 @@
-import React, {
-	useState,
-	useEffect,
-} from "react";
+import React, { useState, useEffect } from "react";
 import "./style/main.css";
-import { GiShoppingBag } from "react-icons/gi";
-import RatingStars from "./components/RatingStars";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HiShoppingCart } from "react-icons/hi";
 import ShoppingCart from "./components/ShoppingCart";
+
 
 const products = [
 	{
 		id: 1,
-		name: "Vivamus vitae",
-		rating: 4.3,
-		description:
-			"Vivamus vitae neque accumsan, ultrices nisl et, viverra magna. Fusce nec maximus sem.",
-		price: 199,
-		image: require("./assets/images/product-1.png"),
+		name: "Macbook Air",
+		price: 1299,
+		image: require("./assets/images/macbook.jpg"),
 	},
 	{
 		id: 2,
-		name: "Fusce sit amet ipsum",
-		rating: 4.2,
-		description:
-			"Maecenas fermentum urna egestas urna ullamcorper sodales. Sed a enim imperdiet, tempus massa a, iaculis tellus.",
-		price: 229,
-		image: require("./assets/images/product-2.png"),
+		name: "iPhone 14 Pro",
+		price: 999,
+		image: require("./assets/images/iphone.jpg"),
 	},
 	{
 		id: 3,
-		name: "Etiam volutpat aliquam",
-		rating: 3.2,
-		description:
-			"Praesent et orci vel nunc interdum aliquet et non dolor. Etiam eget finibus justo",
-		price: 99,
-		image: require("./assets/images/product-3.png"),
-	},
-	{
-		id: 4,
-		name: "Lorem ipsum dolor",
-		rating: 4.8,
-		description:
-			"Duis nibh sapien, placerat non nulla ac, suscipit laoreet tortor.",
-		price: 119,
-		image: require("./assets/images/product-4.png"),
-	},
-	{
-		id: 5,
-		name: "Ultrices nisl",
-		rating: 4.5,
-		description:
-			"Phasellus condimentum, ante et dictum placerat, nulla ipsum commodo lorem, ut mollis nibh turpis a metus.",
-		price: 85,
-		image: require("./assets/images/product-5.jpg"),
-	},
-	{
-		id: 6,
-		name: "Curabitur in elementum tortor",
-		rating: 3.8,
-		description:
-			" Mauris convallis diam nibh, non malesuada enim facilisis non. Etiam sapien augue, molestie a porta sed",
-		price: 149,
-		image: require("./assets/images/product-6.png"),
+		name: "Apple Watch",
+		price: 499,
+		image: require("./assets/images/applewatch.jpg"),
 	},
 ];
 
+
+
 function App() {
+
+	const [username, setUsername] = useState('');
+	const [phonenum, setPhonenum] = useState('');
+	const [email, setEmail] = useState('');
+	const [mailingList, setMailingList] = useState(false);
+	
+	
+	const Register = () => {
+		return (
+			<div>
+				<form className="regisForm">
+					<p>Name</p>
+					<input type="text" placeholder="John Doe" required></input>
+					<p>Phone Number</p>
+					<input type="text" placeholder="123-456-7890" required></input>
+					<p>Email Address</p>
+					<input type="email"  placeholder="example@exmaple.com" required></input>
+					<p>Join mailing list?</p>
+					<div className="radios">
+						<p>Yes</p>
+						<input type="radio" id="yes" name="fav_language" value="Yes"></input>
+						<p>No</p>
+						<input type="radio" id="no" name="fav_language" value="No"></input>
+					</div>
+					<button className="submitButton" type="submit">Submit</button>
+				</form>
+			</div>
+		);
+	};
+
+	const Home = () => {
+		return (
+			<main>
+				<h2 className="title">
+					Products
+				</h2>
+				<div className="products">
+					{products.map((product) => (
+						<div
+							className="product"
+							key={product.id}>
+							<img
+								className="product-image"
+								src={
+									product.image
+								}
+								alt={
+									product.image
+								}
+							/>
+							<h4 className="product-name">
+								{product.name}
+							</h4>
+							<span className="product-price">
+								{product.price}$
+							</span>
+							<div className="buttons">
+								<button
+									className="btn"
+									onClick={() =>
+										addProductToCart(
+											product
+										)
+									}>
+									Add to cart
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+		);
+	};
+
 	const [cartsVisibilty, setCartVisible] =
 		useState(false);
 	const [productsInCart, setProducts] =
@@ -125,7 +163,34 @@ function App() {
 	};
 
 	return (
+
+
 		<div className="App">
+			<BrowserRouter>
+				<div className="navbar">
+					<Link to="/home"><h3 className="logo">Home </h3></Link>
+					<Link to="/register"><h2>Register</h2></Link>
+					<button
+						className="btn shopping-cart-btn"
+						onClick={() =>
+							setCartVisible(true)
+						}>
+						<HiShoppingCart size={30} />
+						{productsInCart.length >
+							0 && (
+								<span className="product-count">
+									{
+										productsInCart.length
+									}
+								</span>
+							)}
+					</button>
+				</div>
+				<Routes>
+					<Route path="/home" element={<Home />}></Route>
+					<Route path="/register" element={<Register />}></Route>
+				</Routes>
+			</BrowserRouter>
 			<ShoppingCart
 				visibilty={cartsVisibilty}
 				products={productsInCart}
@@ -137,76 +202,8 @@ function App() {
 				}
 				onProductRemove={onProductRemove}
 			/>
-			<div className="navbar">
-				<h3 className="logo">Logo</h3>
-				<button
-					className="btn shopping-cart-btn"
-					onClick={() =>
-						setCartVisible(true)
-					}>
-					<GiShoppingBag size={24} />
-					{productsInCart.length >
-						0 && (
-						<span className="product-count">
-							{
-								productsInCart.length
-							}
-						</span>
-					)}
-				</button>
-			</div>
-			<main>
-				<h2 className="title">
-					Products
-				</h2>
-				<div className="products">
-					{products.map((product) => (
-						<div
-							className="product"
-							key={product.id}>
-							<img
-								className="product-image"
-								src={
-									product.image
-								}
-								alt={
-									product.image
-								}
-							/>
-							<h4 className="product-name">
-								{product.name}
-							</h4>
-							<RatingStars
-								rating={
-									product.rating
-								}
-							/>
-							<p>
-								{
-									product.description
-								}
-							</p>
-							<span className="product-price">
-								{product.price}$
-							</span>
-							<div className="buttons">
-								<button className="btn">
-									Detail
-								</button>
-								<button
-									className="btn"
-									onClick={() =>
-										addProductToCart(
-											product
-										)
-									}>
-									Add to cart
-								</button>
-							</div>
-						</div>
-					))}
-				</div>
-			</main>
+
+
 		</div>
 	);
 }
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "../style/shoppingCart.css";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { RiDeleteBin6Line } from "react-icons/ri";
+import { HiTrash } from "react-icons/hi";
 
 function ShoppingCart({
 	visibilty,
 	products,
 	onProductRemove,
 	onClose,
-	onQuantityChange,
 }) {
 	return (
 		<div
@@ -20,20 +19,17 @@ function ShoppingCart({
 			}}>
 			<div className="shoppingCart">
 				<div className="header">
-					<h2>Shopping cart</h2>
+					<h2>Cart</h2>
 					<button
 						className="btn close-btn"
 						onClick={onClose}>
-						<AiFillCloseCircle
-							size={30}
-						/>
+						<AiFillCloseCircle size={30}/>
 					</button>
 				</div>
 				<div className="cart-products">
 					{products.length === 0 && (
 						<span className="empty-text">
-							Your basket is
-							currently empty
+							No items in your order.
 						</span>
 					)}
 					{products.map((product) => (
@@ -51,67 +47,19 @@ function ShoppingCart({
 									{product.name}
 								</h3>
 								<span className="product-price">
-									{product.price *
-										product.count}
-									$
+									{product.price}$
 								</span>
 							</div>
-							<select
-								className="count"
-								value={
-									product.count
-								}
-								onChange={(
-									event
-								) => {
-									onQuantityChange(
-										product.id,
-										event
-											.target
-											.value
-									);
-								}}>
-								{[
-									...Array(
-										10
-									).keys(),
-								].map(
-									(number) => {
-										const num =
-											number +
-											1;
-										return (
-											<option
-												value={
-													num
-												}
-												key={
-													num
-												}>
-												{
-													num
-												}
-											</option>
-										);
-									}
-								)}
-							</select>
 							<button
 								className="btn remove-btn"
-								onClick={() =>
-									onProductRemove(
-										product
-									)
-								}>
-								<RiDeleteBin6Line
-									size={20}
-								/>
+								onClick={() => onProductRemove(product)}>
+								<HiTrash size={30}/>
 							</button>
 						</div>
 					))}
 					{products.length > 0 && (
 						<button className="btn checkout-btn">
-							Proceed to checkout
+							Checkout
 						</button>
 					)}
 				</div>
